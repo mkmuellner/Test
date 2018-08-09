58b4d9de4dad0e8ed3dea3afcbae6312f2440f2d
mkmuellner <mkmuellner@gmail.com>
2018-08-09 09:15:01 +0100

    Update GAN_clone.py

--- GAN_clone.py
+++ GAN_clone.py
@@ -7,10 +7,12 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+import matplotlib.pyplot
+
 from torch.autograd import Variable
 
 # Data params
-data_mean = 4
+data_mean = 40
 data_stddev = 1.25
 
 # Model params
@@ -25,10 +27,10 @@ minibatch_size = d_input_size
 d_learning_rate = 2e-4  # 2e-4
 g_learning_rate = 2e-4
 optim_betas = (0.9, 0.999)
-num_epochs = 30000
-print_interval = 200
+num_epochs = 300000
+print_interval = 30000
 d_steps = 1  # 'k' steps in the original GAN paper. Can put the discriminator on higher training freq than generator
-g_steps = 1
+g_steps = 5
 
 # ### Uncomment only one of these
 #(name, preprocess, d_input_func) = ("Raw data", lambda data: data, lambda x: x)
@@ -122,9 +124,14 @@ for epoch in range(num_epochs):
         g_optimizer.step()  # Only optimizes G's parameters
 
     if epoch % print_interval == 0:
-        print("%s: D: %s/%s G: %s (Real: %s, Fake: %s) " % (epoch,
+        print(epoch)
+        """"print("%s: D: %s/%s G: %s (Real: %s, Fake: %s) " % (epoch,
                                                             extract(d_real_error)[0],
                                                             extract(d_fake_error)[0],
                                                             extract(g_error)[0],
                                                             stats(extract(d_real_data)),
-                                                            stats(extract(d_fake_data))))
+                                                            stats(extract(d_fake_data))))# -*- coding: utf-8 -*-"""
+matplotlib.pyplot.hist(extract(d_fake_data), histtype = "step", bins = 20)
+#matplotlib.pyplot.hist(extract(d_real_data), histtype = "step", col = "")
+matplotlib.pyplot.figure()
+matplotlib.pyplot.hist(extract(d_real_data), histtype = "step", color = "orange", bins = 20)
