2dbfe79a9c6e58a4135cc69a9b38234ec4f02df7
mkmuellner <mkmuellner@gmail.com>
2018-08-13 13:16:49 +0100

    Update GAN_clone.py

--- GAN_clone.py
+++ GAN_clone.py
@@ -1,130 +1,339 @@
-#!/usr/bin/env python
+'''
+An example of distribution approximation using Generative Adversarial Networks
+in TensorFlow.
 
-# Generative Adversarial Networks (GAN) example in PyTorch.
-# See related blog post at https://medium.com/@devnag/generative-adversarial-networks-gans-in-50-lines-of-code-pytorch-e81b79659e3f#.sch4xgsa9
+Based on the blog post by Eric Jang:
+http://blog.evjang.com/2016/06/generative-adversarial-nets-in.html,
+
+and of course the original GAN paper by Ian Goodfellow et. al.:
+https://arxiv.org/abs/1406.2661.
+
+The minibatch discrimination technique is taken from Tim Salimans et. al.:
+https://arxiv.org/abs/1606.03498.
+'''
+
+import argparse
 import numpy as np
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-from torch.autograd import Variable
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from matplotlib import animation
+import seaborn as sns
 
-# Data params
-data_mean = 4
-data_stddev = 1.25
+sns.set(color_codes=True)
 
-# Model params
-g_input_size = 1     # Random noise dimension coming into generator, per output vector
-g_hidden_size = 50   # Generator complexity
-g_output_size = 1    # size of generated output vector
-d_input_size = 100   # Minibatch size - cardinality of distributions
-d_hidden_size = 50   # Discriminator complexity
-d_output_size = 1    # Single dimension for 'real' vs. 'fake'
-minibatch_size = d_input_size
+seed = 42
+np.random.seed(seed)
+tf.set_random_seed(seed)
 
-d_learning_rate = 2e-4  # 2e-4
-g_learning_rate = 2e-4
-optim_betas = (0.9, 0.999)
-num_epochs = 30000
-print_interval = 200
-d_steps = 1  # 'k' steps in the original GAN paper. Can put the discriminator on higher training freq than generator
-g_steps = 1
 
-# ### Uncomment only one of these
-#(name, preprocess, d_input_func) = ("Raw data", lambda data: data, lambda x: x)
-(name, preprocess, d_input_func) = ("Data and variances", lambda data: decorate_with_diffs(data, 2.0), lambda x: x * 2)
+class DataDistribution(object):
+    def __init__(self):
+        self.mu = 4
+        self.sigma = 0.5
 
-print("Using data [%s]" % (name))
+    def sample(self, N):
+        samples = np.random.normal(self.mu, self.sigma, N)
+        samples.sort()
+        return samples
 
-# ##### DATA: Target data and generator input data
 
-def get_distribution_sampler(mu, sigma):
-    return lambda n: torch.Tensor(np.random.normal(mu, sigma, (1, n)))  # Gaussian
+class GeneratorDistribution(object):
+    def __init__(self, range):
+        self.range = range
 
-def get_generator_input_sampler():
-    return lambda m, n: torch.rand(m, n)  # Uniform-dist data into generator, _NOT_ Gaussian
+    def sample(self, N):
+        return np.linspace(-self.range, self.range, N) + \
+            np.random.random(N) * 0.01
 
-# ##### MODELS: Generator model and discriminator model
 
-class Generator(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size):
-        super(Generator, self).__init__()
-        self.map1 = nn.Linear(input_size, hidden_size)
-        self.map2 = nn.Linear(hidden_size, hidden_size)
-        self.map3 = nn.Linear(hidden_size, output_size)
+def linear(input, output_dim, scope=None, stddev=1.0):
+    with tf.variable_scope(scope or 'linear'):
+        w = tf.get_variable(
+            'w',
+            [input.get_shape()[1], output_dim],
+            initializer=tf.random_normal_initializer(stddev=stddev)
+        )
+        b = tf.get_variable(
+            'b',
+            [output_dim],
+            initializer=tf.constant_initializer(0.0)
+        )
+        return tf.matmul(input, w) + b
 
-    def forward(self, x):
-        x = F.elu(self.map1(x))
-        x = F.sigmoid(self.map2(x))
-        return self.map3(x)
 
-class Discriminator(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size):
-        super(Discriminator, self).__init__()
-        self.map1 = nn.Linear(input_size, hidden_size)
-        self.map2 = nn.Linear(hidden_size, hidden_size)
-        self.map3 = nn.Linear(hidden_size, output_size)
+def generator(input, h_dim):
+    h0 = tf.nn.softplus(linear(input, h_dim, 'g0'))
+    h1 = linear(h0, 1, 'g1')
+    return h1
 
-    def forward(self, x):
-        x = F.elu(self.map1(x))
-        x = F.elu(self.map2(x))
-        return F.sigmoid(self.map3(x))
 
-def extract(v):
-    return v.data.storage().tolist()
+def discriminator(input, h_dim, minibatch_layer=True):
+    h0 = tf.nn.relu(linear(input, h_dim * 2, 'd0'))
+    h1 = tf.nn.relu(linear(h0, h_dim * 2, 'd1'))
 
-def stats(d):
-    return [np.mean(d), np.std(d)]
+    # without the minibatch layer, the discriminator needs an additional layer
+    # to have enough capacity to separate the two distributions correctly
+    if minibatch_layer:
+        h2 = minibatch(h1)
+    else:
+        h2 = tf.nn.relu(linear(h1, h_dim * 2, scope='d2'))
 
-def decorate_with_diffs(data, exponent):
-    mean = torch.mean(data.data, 1, keepdim=True)
-    mean_broadcast = torch.mul(torch.ones(data.size()), mean.tolist()[0][0])
-    diffs = torch.pow(data - Variable(mean_broadcast), exponent)
-    return torch.cat([data, diffs], 1)
+    h3 = tf.sigmoid(linear(h2, 1, scope='d3'))
+    return h3
 
-d_sampler = get_distribution_sampler(data_mean, data_stddev)
-gi_sampler = get_generator_input_sampler()
-G = Generator(input_size=g_input_size, hidden_size=g_hidden_size, output_size=g_output_size)
-D = Discriminator(input_size=d_input_func(d_input_size), hidden_size=d_hidden_size, output_size=d_output_size)
-criterion = nn.BCELoss()  # Binary cross entropy: http://pytorch.org/docs/nn.html#bceloss
-d_optimizer = optim.Adam(D.parameters(), lr=d_learning_rate, betas=optim_betas)
-g_optimizer = optim.Adam(G.parameters(), lr=g_learning_rate, betas=optim_betas)
 
-for epoch in range(num_epochs):
-    for d_index in range(d_steps):
-        # 1. Train D on real+fake
-        D.zero_grad()
+def minibatch(input, num_kernels=5, kernel_dim=3):
+    x = linear(input, num_kernels * kernel_dim, scope='minibatch', stddev=0.02)
+    activation = tf.reshape(x, (-1, num_kernels, kernel_dim))
+    diffs = tf.expand_dims(activation, 3) - \
+        tf.expand_dims(tf.transpose(activation, [1, 2, 0]), 0)
+    abs_diffs = tf.reduce_sum(tf.abs(diffs), 2)
+    minibatch_features = tf.reduce_sum(tf.exp(-abs_diffs), 2)
+    return tf.concat([input, minibatch_features], 1)
 
-        #  1A: Train D on real
-        d_real_data = Variable(d_sampler(d_input_size))
-        d_real_decision = D(preprocess(d_real_data))
-        d_real_error = criterion(d_real_decision, Variable(torch.ones(1)))  # ones = true
-        d_real_error.backward() # compute/store gradients, but don't change params
 
-        #  1B: Train D on fake
-        d_gen_input = Variable(gi_sampler(minibatch_size, g_input_size))
-        d_fake_data = G(d_gen_input).detach()  # detach to avoid training G on these labels
-        d_fake_decision = D(preprocess(d_fake_data.t()))
-        d_fake_error = criterion(d_fake_decision, Variable(torch.zeros(1)))  # zeros = fake
-        d_fake_error.backward()
-        d_optimizer.step()     # Only optimizes D's parameters; changes based on stored gradients from backward()
+def optimizer(loss, var_list):
+    learning_rate = 0.001
+    step = tf.Variable(0, trainable=False)
+    optimizer = tf.train.AdamOptimizer(learning_rate).minimize(
+        loss,
+        global_step=step,
+        var_list=var_list
+    )
+    return optimizer
 
-    for g_index in range(g_steps):
-        # 2. Train G on D's response (but DO NOT train D on these labels)
-        G.zero_grad()
 
-        gen_input = Variable(gi_sampler(minibatch_size, g_input_size))
-        g_fake_data = G(gen_input)
-        dg_fake_decision = D(preprocess(g_fake_data.t()))
-        g_error = criterion(dg_fake_decision, Variable(torch.ones(1)))  # we want to fool, so pretend it's all genuine
+def log(x):
+    '''
+    Sometimes discriminiator outputs can reach values close to
+    (or even slightly less than) zero due to numerical rounding.
+    This just makes sure that we exclude those values so that we don't
+    end up with NaNs during optimisation.
+    '''
+    return tf.log(tf.maximum(x, 1e-5))
 
-        g_error.backward()
-        g_optimizer.step()  # Only optimizes G's parameters
 
-    if epoch % print_interval == 0:
-        print("%s: D: %s/%s G: %s (Real: %s, Fake: %s) " % (epoch,
-                                                            extract(d_real_error)[0],
-                                                            extract(d_fake_error)[0],
-                                                            extract(g_error)[0],
-                                                            stats(extract(d_real_data)),
-                                                            stats(extract(d_fake_data))))
+class GAN(object):
+    def __init__(self, params):
+        # This defines the generator network - it takes samples from a noise
+        # distribution as input, and passes them through an MLP.
+        with tf.variable_scope('G'):
+            self.z = tf.placeholder(tf.float32, shape=(params.batch_size, 1))
+            self.G = generator(self.z, params.hidden_size)
+
+        # The discriminator tries to tell the difference between samples from
+        # the true data distribution (self.x) and the generated samples
+        # (self.z).
+        #
+        # Here we create two copies of the discriminator network
+        # that share parameters, as you cannot use the same network with
+        # different inputs in TensorFlow.
+        self.x = tf.placeholder(tf.float32, shape=(params.batch_size, 1))
+        with tf.variable_scope('D'):
+            self.D1 = discriminator(
+                self.x,
+                params.hidden_size,
+                params.minibatch
+            )
+        with tf.variable_scope('D', reuse=True):
+            self.D2 = discriminator(
+                self.G,
+                params.hidden_size,
+                params.minibatch
+            )
+
+        # Define the loss for discriminator and generator networks
+        # (see the original paper for details), and create optimizers for both
+        self.loss_d = tf.reduce_mean(-log(self.D1) - log(1 - self.D2))
+        self.loss_g = tf.reduce_mean(-log(self.D2))
+
+        vars = tf.trainable_variables()
+        self.d_params = [v for v in vars if v.name.startswith('D/')]
+        self.g_params = [v for v in vars if v.name.startswith('G/')]
+
+        self.opt_d = optimizer(self.loss_d, self.d_params)
+        self.opt_g = optimizer(self.loss_g, self.g_params)
+
+
+def train(model, data, gen, params):
+    anim_frames = []
+
+    with tf.Session() as session:
+        tf.local_variables_initializer().run()
+        tf.global_variables_initializer().run()
+
+        for step in range(params.num_steps + 1):
+            # update discriminator
+            x = data.sample(params.batch_size)
+            z = gen.sample(params.batch_size)
+            loss_d, _, = session.run([model.loss_d, model.opt_d], {
+                model.x: np.reshape(x, (params.batch_size, 1)),
+                model.z: np.reshape(z, (params.batch_size, 1))
+            })
+
+            # update generator
+            z = gen.sample(params.batch_size)
+            loss_g, _ = session.run([model.loss_g, model.opt_g], {
+                model.z: np.reshape(z, (params.batch_size, 1))
+            })
+
+            if step % params.log_every == 0:
+                print('{}: {:.4f}\t{:.4f}'.format(step, loss_d, loss_g))
+
+            if params.anim_path and (step % params.anim_every == 0):
+                anim_frames.append(
+                    samples(model, session, data, gen.range, params.batch_size)
+                )
+
+        if params.anim_path:
+            save_animation(anim_frames, params.anim_path, gen.range)
+        else:
+            samps = samples(model, session, data, gen.range, params.batch_size)
+            plot_distributions(samps, gen.range)
+
+
+def samples(
+    model,
+    session,
+    data,
+    sample_range,
+    batch_size,
+    num_points=10000,
+    num_bins=100
+):
+    '''
+    Return a tuple (db, pd, pg), where db is the current decision
+    boundary, pd is a histogram of samples from the data distribution,
+    and pg is a histogram of generated samples.
+    '''
+    xs = np.linspace(-sample_range, sample_range, num_points)
+    bins = np.linspace(-sample_range, sample_range, num_bins)
+
+    # decision boundary
+    db = np.zeros((num_points, 1))
+    for i in range(num_points // batch_size):
+        db[batch_size * i:batch_size * (i + 1)] = session.run(
+            model.D1,
+            {
+                model.x: np.reshape(
+                    xs[batch_size * i:batch_size * (i + 1)],
+                    (batch_size, 1)
+                )
+            }
+        )
+
+    # data distribution
+    d = data.sample(num_points)
+    pd, _ = np.histogram(d, bins=bins, density=True)
+
+    # generated samples
+    zs = np.linspace(-sample_range, sample_range, num_points)
+    g = np.zeros((num_points, 1))
+    for i in range(num_points // batch_size):
+        g[batch_size * i:batch_size * (i + 1)] = session.run(
+            model.G,
+            {
+                model.z: np.reshape(
+                    zs[batch_size * i:batch_size * (i + 1)],
+                    (batch_size, 1)
+                )
+            }
+        )
+    pg, _ = np.histogram(g, bins=bins, density=True)
+
+    return db, pd, pg
+
+
+def plot_distributions(samps, sample_range):
+    db, pd, pg = samps
+    db_x = np.linspace(-sample_range, sample_range, len(db))
+    p_x = np.linspace(-sample_range, sample_range, len(pd))
+    f, ax = plt.subplots(1)
+    ax.plot(db_x, db, label='decision boundary')
+    ax.set_ylim(0, 1)
+    plt.plot(p_x, pd, label='real data')
+    plt.plot(p_x, pg, label='generated data')
+    plt.title('1D Generative Adversarial Network')
+    plt.xlabel('Data values')
+    plt.ylabel('Probability density')
+    plt.legend()
+    plt.show()
+
+
+def save_animation(anim_frames, anim_path, sample_range):
+    f, ax = plt.subplots(figsize=(6, 4))
+    f.suptitle('1D Generative Adversarial Network', fontsize=15)
+    plt.xlabel('Data values')
+    plt.ylabel('Probability density')
+    ax.set_xlim(-6, 6)
+    ax.set_ylim(0, 1.4)
+    line_db, = ax.plot([], [], label='decision boundary')
+    line_pd, = ax.plot([], [], label='real data')
+    line_pg, = ax.plot([], [], label='generated data')
+    frame_number = ax.text(
+        0.02,
+        0.95,
+        '',
+        horizontalalignment='left',
+        verticalalignment='top',
+        transform=ax.transAxes
+    )
+    ax.legend()
+
+    db, pd, _ = anim_frames[0]
+    db_x = np.linspace(-sample_range, sample_range, len(db))
+    p_x = np.linspace(-sample_range, sample_range, len(pd))
+
+    def init():
+        line_db.set_data([], [])
+        line_pd.set_data([], [])
+        line_pg.set_data([], [])
+        frame_number.set_text('')
+        return (line_db, line_pd, line_pg, frame_number)
+
+    def animate(i):
+        frame_number.set_text(
+            'Frame: {}/{}'.format(i, len(anim_frames))
+        )
+        db, pd, pg = anim_frames[i]
+        line_db.set_data(db_x, db)
+        line_pd.set_data(p_x, pd)
+        line_pg.set_data(p_x, pg)
+        return (line_db, line_pd, line_pg, frame_number)
+
+    anim = animation.FuncAnimation(
+        f,
+        animate,
+        init_func=init,
+        frames=len(anim_frames),
+        blit=True
+    )
+    anim.save(anim_path, fps=30, extra_args=['-vcodec', 'libx264'])
+
+
+def main(args):
+    model = GAN(args)
+    train(model, DataDistribution(), GeneratorDistribution(range=8), args)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--num-steps', type=int, default=5000,
+                        help='the number of training steps to take')
+    parser.add_argument('--hidden-size', type=int, default=4,
+                        help='MLP hidden size')
+    parser.add_argument('--batch-size', type=int, default=8,
+                        help='the batch size')
+    parser.add_argument('--minibatch', action='store_true',
+                        help='use minibatch discrimination')
+    parser.add_argument('--log-every', type=int, default=10,
+                        help='print loss after this many steps')
+    parser.add_argument('--anim-path', type=str, default=None,
+                        help='path to the output animation file')
+    parser.add_argument('--anim-every', type=int, default=1,
+                        help='save every Nth frame for animation')
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    main(parse_args())
